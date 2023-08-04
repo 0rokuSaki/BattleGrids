32a064f5ae3bef8ba161fa6a10cfb472b2e15e70
Omer <omerrbarr@gmail.com>
2023-08-04 10:27:00 -0400

    feature added DBConnection33

--- src/Server/DBManager.java
+++ src/Server/DBManager.java
@@ -5,37 +5,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public interface DBManager {
-
-
-
-
-
-
     public void deleteUsersTable() throws SQLException;
-
-
-
-
     public void createUsersTable() throws ClassNotFoundException, SQLException;
     public void printUsersTable() throws SQLException;
-
-
-
     public void addUser(String username, String password) throws SQLException;
-
-
-
     public void setUserName(String username);
     public void setPassword(String password);
     public String getPassword() throws SQLException;
     public boolean isUserNameExist(String username) throws SQLException;
     public boolean validLogIn(String password);
-
-
-
-
-
-
-
 
 }
