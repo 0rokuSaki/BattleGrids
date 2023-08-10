f7fd86901223a3c5098160ad334a825134a0261f
Aaron Barkan <aaron.brkn@gmail.com>
2023-08-10 19:48:10 +0300

    refactor: Removed 'Friends' functionality

--- src/Client/Controllers/MyProfileMenuController.java
+++ src/Client/Controllers/MyProfileMenuController.java
@@ -1,17 +1,20 @@
 package Client.Controllers;
 
-
-import Client.Controllers.ControllerBase;
+import Client.ClientImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class MyProfileMenuController extends ControllerBase {
 
     @FXML
-    void myFriendsButtonPressed(ActionEvent event) {
+    private Label userLabel;
 
+    @FXML
+    void initialize() {
+        userLabel.setText("User: " + ClientImpl.getInstance().getUsername());
     }
 
     @FXML
